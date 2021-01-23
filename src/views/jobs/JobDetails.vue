3c96c1a542ae85b056d5b4a951087fa5724920b8
“Debora <debora.crescenzo@gmail.com>
2021-01-23 17:19:16 +0000

    done

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,17 +1,30 @@
 <template>
   <h1>Job details page</h1>
+  <div v-if="job">
+  <p>{{job.title}}</p>
   <p>The job id is {{ id }} </p> <!-- see App.vue -->
+  <p>{{job.details}} </p> <!-- see App.vue -->
+  </div>
+  <div v-else>
+      <p>Loading...</p>
+  </div>
 </template>
 
 <script>
 
 export default {
   props: ['id'],
-  // data() {
-  //   return {
-  //     id: this.$route.params.id
-  //   }
-  // }
+  data(){
+    return {
+        job: null
+    }
+  },
+  mounted(){
+      fetch('http://localhost:3000/jobs/' + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+  }
 }
 </script>
 
